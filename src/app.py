e402abc28e04100e129226d477c2da8cb149776e
fux <jan.dick@bdp.org>
2023-11-30 03:24:31 +0100

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def main(): 
+    return render_template("index.html")
+
+@app.route("/data")
+def data(): 
+    return render_template("data.html")
+
+@app.route("/upload")
+def upload(): 
+    return render_template("upload.html")
+
+
+if __name__ == "__main__": 
+    app.run(debug=True)
